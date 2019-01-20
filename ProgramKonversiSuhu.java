9c99bba8ea9d04c9899829a2468d64ad59548371
Sava Athariq <sava.athariq93@gmail.com>
2019-01-20 21:42:15 +0700

    Ubah file

--- ProgramKonversiSuhu.java
+++ ProgramKonversiSuhu.java
@@ -15,7 +15,6 @@ public class ProgramKonversiSuhu {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Frame_program_konversi_suhu Laund = new Frame_program_konversi_suhu();
-        Laund.setVisible(true);
+	System.out.println("Hello World");
     }
 }
